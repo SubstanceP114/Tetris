ba2e15f43f166f50610b2def80ea5c30ed14c854
SubstanceP <3161387475@qq.com>
2025-02-03 12:23:40 +0800

    feat: 新增Renderer::Draw方法

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -4,3 +4,11 @@ void Renderer::Clear()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 }
+
+void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader)
+{
+	shader.Bind();
+	va.Bind();
+	ib.Bind();
+	glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
+}
--- src/Renderer.h
+++ src/Renderer.h
@@ -3,9 +3,14 @@
 #include <glew/glew.h>
 #include <glfw/glfw3.h>
 
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
+
 static class Renderer
 {
 public:
 	static void Clear();
+	static void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader);
 };
 
